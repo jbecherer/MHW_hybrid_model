94efe646a7eea190b4e1bf3935861aebf3436831
jbecherer <jbecherer@gmx.net>
2025-06-13 14:53:56 +0200

    now the plottin is only on the validation years not the test years

--- code_plot/plot_compare_model_variance.py
+++ code_plot/plot_compare_model_variance.py
@@ -22,8 +22,10 @@ def plot_variance_correlation(region='NorthSea', csv='models.csv'):
     # load validation data
     #==============================================================================
 
-    tensor_in = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_input_data_valtest.csv')
-    tensor_out = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_output_data_valtest.csv')
+    # tensor_in = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_input_data_valtest.csv')
+    # tensor_out = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_output_data_valtest.csv')
+    tensor_in = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_input_data_val.csv')
+    tensor_out = aisst.load_tensor_from_csv('../data/ml_training/' + region + '/ml_output_data_val.csv')
 
     input_mean, input_std, output_mean, output_std = aisst.load_normalization_parameters(region)
 
