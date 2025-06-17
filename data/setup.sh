b020057668c4c70be777ea39b4654fcbc32e0554
jbecherer <jbecherer@gmx.net>
2025-06-17 20:23:00 +0200

    made note which MPI files to download

--- data/setup.sh
+++ data/setup.sh
@@ -2,3 +2,10 @@
 #
 #
 # mkdir -p ./ml_training
+#
+# download mpi data:
+#`./data/mpi/mon/`
+#  - `aisst_histssp585.nc` 
+#  - `aisst_histssp126.nc`
+#  - `aisst_ssp245.nc` 
+#  - `aisst_ssp370.nc`
