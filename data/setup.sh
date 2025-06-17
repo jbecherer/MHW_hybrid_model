6fd7a7a4a5075dea5ba9d6bb81031f2f7f3ade57
jbecherer <jbecherer@gmx.net>
2025-06-17 20:47:11 +0200

    add folders for the hybrid model

--- data/setup.sh
+++ data/setup.sh
@@ -3,9 +3,17 @@
 #
 # mkdir -p ./ml_training
 #
+#
 # download mpi data:
 #`./data/mpi/mon/`
 #  - `aisst_histssp585.nc` 
 #  - `aisst_histssp126.nc`
 #  - `aisst_ssp245.nc` 
 #  - `aisst_ssp370.nc`
+#
+#
+mkdir -p ./hybrid_model
+mkdir -p ./hybrid_model/histssp585
+mkdir -p ./hybrid_model/ssp126
+mkdir -p ./hybrid_model/ssp245
+mkdir -p ./hybrid_model/ssp370
