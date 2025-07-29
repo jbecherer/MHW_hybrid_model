cd2d1b3271ea057a4aa0fddf28accae7d727898b
jbecherer <jbecherer@gmx.net>
2025-07-29 17:33:06 +0200

    merge of future mhw calculations

---
+++ code_proc/merge_latof_mhw_return.py
@@ -0,0 +1,28 @@
+# This script merges the mhw stats for the different latitude bands for each scenario
+
+
+import numpy as np
+import pandas as pd
+import os
+import glob
+import sys
+import xarray as xr
+
+# Load in the data
+idir = '../data/mhw/'
+
+scenarios = ['histssp585', 'ssp126', 'ssp245', 'ssp370']
+
+for i in range(len(scenarios)):
+    scenario = scenarios[i]
+
+    # loop through all files in the directory
+    files = glob.glob(idir + 'return_' +  scenario + '_la*.nc')
+    ds = xr.open_dataset(files[0])
+    for j in range(1, len(files)):
+        ds2 = xr.open_dataset(files[j])
+        ds = xr.merge([ds, ds2])
+
+    ds.to_netcdf('../data/mhw/merged_return_' + scenario + '.nc')
+
+
